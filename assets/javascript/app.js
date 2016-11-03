b413cea65a80b5b97fc2cc59661c6cd87f282ec8
Kaysic <michaeldaq@mail.com>
2016-11-03 15:58:27 -0400

    added a few questions and setup for timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,12 +3,16 @@ $(document).ready(function(){
 		var questions = [{
 			question: "What is Captain America's Shield made out of in the MCU",
 			choices: ["Vibranium", "Adamantium", "A combination of both", "Mithril"],
-			correct: "Vibranium"},{
+			correct: 0},{
 			question: "Who are what is found inside of the caccoon in the Collectors in Guardian of the Galaxy",
 			choices: ["Quicksilver", "Howard the Duck", "Galactus", "Adam Warlock"],
-			correct: "Adam Warlock"
-		}
-		]
-	
-	}
-})
+			correct: 4
+		}];
+
+		var questionNumber = 0;
+		var userChoice = [];
+		var  trivia = $('.question');
+
+
+
+	});
