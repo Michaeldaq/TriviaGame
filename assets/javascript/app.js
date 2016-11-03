8a03017ebd3577213fe5de0b65359e5c1f7e1602
Kaysic <michaeldaq@mail.com>
2016-11-03 13:44:10 -0400

    added question var

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	//Created Object with questions, choices and answers for game
+		var questions = [{
+			question: "What is Captain America's Shield made out of in the MCU",
+			choices: ["Vibranium", "Adamantium", "A combination of both", "Mithril"],
+			correct: "Vibranium"},{
+			question: "Who are what is found inside of the caccoon in the Collectors in Guardian of the Galaxy",
+			choices: ["Quicksilver", "Howard the Duck", "Galactus", "Adam Warlock"],
+			correct: "Adam Warlock"
+		}
+		]
+	
+	}
+})
